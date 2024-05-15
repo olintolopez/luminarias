84fe71494f1cd1daf772b1b6420a8a524d9f868a
Olinto Lopez <34415910+olintolopez@users.noreply.github.com>
2024-05-15 02:17:03 -0400

    El array de uuarios
    El archivo que users.js tiene los usuarios que pueden usar la interfaz

---
+++ storage/users.js
@@ -0,0 +1,15 @@
+let users=[
+    { 
+        id:1,
+        fullname: "Olinto Lopez", 
+        username: "olintolopez",
+        password:"123456"
+    },
+    { 
+      id:2,
+      fullname: "Francis Medina", 
+      username: "francisMedina",
+      password:"francis456"
+    }
+
+]
